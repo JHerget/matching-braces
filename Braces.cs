f4b928bf3b93787c38b0a85d315542a797ee42d0
JHerget <lethalbacon16@gmail.com>
2022-03-18 16:35:03 -0600

    Update Braces.cs
    Forgot to add my comments

--- Braces.cs
+++ Braces.cs
@@ -6,27 +6,27 @@ namespace JobNimbus
     {
         static bool MatchingBraces(string inputString)
         {
-            int openBraces = 0;
+            int openBraces = 0; //keep track of how many open braces we've encountered
 
-            foreach (char c in inputString)
+            foreach (char c in inputString) //loop through each character of the given string
             {
                 if (c.Equals('{'))
                 {
-                    openBraces++;
+                    openBraces++; //increment the number of open braces that need closing braces
                 }
 
                 if (c.Equals('}'))
                 {
-                    if (openBraces <= 0)
+                    if (openBraces <= 0) //if we haven't encountered any open braces then we already know we have mismatched braces
                     {
                         return false;
                     }
 
-                    openBraces--;
+                    openBraces--; //decrement the number of open braces that don't have a closing brace
                 }
             }
 
-            return !(openBraces > 0);
+            return !(openBraces > 0); //if the number of open braces is 0 return true, otherwise false
         }
 
         static void UnitTest(string[] testInputsArray, bool[] testOutputsArray)
